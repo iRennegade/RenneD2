0ef4da3a2f5b1498cceddedb22a52d2b157a2441
iRennegade <81411511+iRennegade@users.noreply.github.com>
2021-11-25 12:17:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const axios = require("axios");
+const nodos = 0
+const prompt = require("prompt-sync")();
 
 (async () => {
-
+  name = prompt("Nombre de usuario de replit : ")
+  
+  try {
+    axios.post("")
+  }
 })();
+
+// No usar
